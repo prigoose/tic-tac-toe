c92632aad486c9c338ae15c82545288c115bbf63
prigoose <priyalghose@gmail.com>
2025-09-22 12:39:25 -0400

    tic tac toe grid displays

---
+++ Tic Tac Toe/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Board from './Board'
+
+function App() {
+
+  return (
+    <>
+      <h1>Tic Tac Toe</h1>
+      <Board />
+    </>
+  )
+}
+
+export default App
---
+++ Tic Tac Toe/src/Board.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import './Board.css'
+
+function App() {
+
+  return (
+    <div className="wrapper">
+        <div className="left top box"></div>
+        <div className="middle-column top box"></div>
+        <div className="right top box"></div>
+        <div className="left middle-row box"></div>
+        <div className="middle-column middle-row box"></div>
+        <div className="right middle-row box"></div>
+        <div className="left bottom box"></div>
+        <div className="middle-column bottom box"></div>
+        <div className="right bottom box"></div>
+    </div>
+  )
+}
+
+export default App
